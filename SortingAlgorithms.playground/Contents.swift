40670fb4eeb9ebbbecc55b51ab206d37d39dd28e
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-05-09 19:12:00 -0700

    add bubblesort

--- SortingAlgorithms.playground/Contents.swift
+++ SortingAlgorithms.playground/Contents.swift
@@ -3,3 +3,20 @@
 import UIKit
 
 var str = "Hello, playground"
+
+func bubbleSort(inout arr:[Int]) {
+    
+    var done = false
+    while !done {
+        done = true
+        for i in 1..<arr.count {
+            if arr[i-1] > arr[i] {
+                (arr[i], arr[i-1]) = (arr[i-1], arr[i])
+                done = false
+            }
+        }
+    }
+}
+
+var myArr = [4, 2, 9, -9, 44, 0, 19]
+bubbleSort(&myArr)
